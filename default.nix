713972ec1c224fdf863f1ac1853fd55e33aa9ce5
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2023-11-13 23:02:40 +0100

    nix: Use mutwo-nix, but with local src

--- default.nix
+++ default.nix
@@ -1,23 +1,11 @@
-with import <nixpkgs> {};
-with pkgs.python3Packages;
-
 let
-
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/97aea97f996973955889630c437ceaea405ea0a7.tar.gz";
-  mutwo-core = import (mutwo-core-archive + "/default.nix");
-
+  sourcesTarball = fetchTarball "https://github.com/mutwo-org/mutwo-nix/archive/refs/heads/main.tar.gz";
+  mutwo.reaper = import (sourcesTarball + "/mutwo.reaper/default.nix") {};
+  mutwo.reaper-local = mutwo.reaper.overrideAttrs (
+    finalAttrs: previousAttrs: {
+       src = ./.;
+    }
+  );
 in
+  mutwo.reaper-local
 
-  buildPythonPackage rec {
-    name = "mutwo.reaper";
-    src = fetchFromGitHub {
-      owner = "mutwo-org";
-      repo = name;
-      rev = "16793da38f743bc5789fe61aada990ae815ced5b";
-      sha256 = "sha256-AstM1sPYjcKFOOPaK0/UySMcA2R4BMNcfSUFCN4qLTA=";
-    };
-    propagatedBuildInputs = [ 
-      mutwo-core
-    ];
-    doCheck = true;
-  }
